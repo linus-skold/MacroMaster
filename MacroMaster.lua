63bc28ef8e5429a05e7124fa170723512611fe86
OriginalDecode <originaldecode@outlook.com>
2020-11-07 02:09:47 +0100

    Can now export and import with a specific profile name instead

--- MacroMaster.lua
+++ MacroMaster.lua
@@ -1,22 +1,50 @@
-local test,addon = ...
+local test, addon = ...
+
 
 local frame = CreateFrame("FRAME")      -- Need a frame to respond to events
 frame:RegisterEvent("ADDON_LOADED")     -- Fired when saved variables are loaded
 frame:RegisterEvent("PLAYER_LOGOUT")    -- Fired when about to log out
 
-local playerClass, englishClass = UnitClass("player")
+local playerClass, englishClass = UnitClass("player") -- Might not need this anymore.
+local playerName = UnitName("player")
+local playerRealm = GetRealmName()
+
+function getKey(characterName, characterRealm) 
+    return characterName .. '-' .. characterRealm
+end
+
 local playerClassMacros = {}
+local playerMacros = {}
+
+local importKey = ""
 
 function frame:OnEvent(event, arg1)
     -- arg1 is whatever addon its loading, by name, has to match the title in the toc
     if event == "ADDON_LOADED" and arg1 == "MacroMaster" then
+        local profile = ""
+        if CharacterProperties ~= nil then
+            profile = CharacterProperties['profile']
+        end
+
         if ClassMacrosDB ~= nil then
-            if ClassMacrosDB[englishClass] ~= nil then
-                playerClassMacros = ClassMacrosDB[englishClass]
+            if ClassMacrosDB[profile] ~= nil then 
+                playerMacros = ClassMacrosDB[profile]
             end
         end
+
+        StaticPopupDialogs["MACRO_MASTER"] = {
+            text = "This action will delete all your current character specific macros! Make sure you back them up if you want to keep them! Do you want to continue?",
+            button1 = "Yes",
+            button2 = "No",
+            OnAccept = function()
+                importMacros(playerMacros)
+            end,
+            timeout = 0,
+            preferredIndex = 3,  -- avoid some UI taint, see http://www.wowace.com/announcements/how-to-avoid-some-ui-taint/
+          }
+
     elseif event == "PLAYER_LOGOUT" then 
-        ClassMacrosDB[englishClass] = playerClassMacros
+        -- ClassMacrosDB[englishClass] = playerClassMacros
     end
 end
 
@@ -25,12 +53,16 @@ frame:SetScript("OnEvent", frame.OnEvent)
 SLASH_MACRO_MASTER1 = "/macromaster"
 SLASH_MACRO_MASTER2 = "/mm"
 
-SlashCmdList["MACRO_MASTER"] = function (arg) 
-    if arg == "export" then
-        playerClassMacros = {}
-        exportMacros(playerClassMacros)
-    elseif arg == "import" then
-        importMacros()
+SlashCmdList["MACRO_MASTER"] = function (args) 
+    local command, profile, realmName = strsplit(" ", args)
+    if command == "export" then
+        playerMacros = {}
+        exportMacros(playerMacros)
+        ClassMacrosDB[profile] = playerMacros
+    elseif command == "import" then
+        startImport(profile, realmName)
+    elseif command == "help" then 
+        help();
     end
 end
 
@@ -75,13 +107,30 @@ function exportMacros(exportTable)
 end
 
 --
-function importMacros() 
+
+function startImport(profile, realmName) 
+    importKey = profile
+    if realmName ~= nil then
+        importKey = getKey(profile, realmName)
+    end
+
+    playerMacros = ClassMacrosDB[importKey]
+   
+   
+   
+    StaticPopup_Show("MACRO_MASTER")
+
+   
+   
+end
+
+function importMacros(importTable) 
+    StaticPopup_Hide("MACRO_MASTER")
     for i=0,18 do
         DeleteMacro(121)
     end
-    
-    for i=1,table.getn(playerClassMacros) do
-        local macro = playerClassMacros[i]
+    for i=1,table.getn(importTable) do
+        local macro = importTable[i]
         local icon = macro[2]
         if type(icon) == 'number' then
             icon = getIcon(icon)
@@ -89,3 +138,10 @@ function importMacros()
         CreateMacro(macro[1], icon, macro[3], 1)
     end
 end
+
+function help() 
+    print('MacroMaster Help')
+    print('/mm import [profile] [ [character name] [character realm] ] -- imports the specified profiles macros')
+    print('/mm export [profile] -- Exports your current characters CHARACTER SPECIFIC macros unless a profile name is specified')
+end
+
